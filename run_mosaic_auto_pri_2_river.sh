d00ad021dbc083db7cb205cb612ba567e6c9b864
Chokurei <guozhilingty@gmail.com>
2021-03-11 20:28:32 +0900

    firt

---
+++ run_mosaic_auto_pri_2_river.sh
@@ -0,0 +1,8 @@
+echo "Running Scripts for model training ..."
+for i in {1}
+do
+    echo "Testing "
+    export CUDA_VISIBLE_DEVICES=2
+    python predict_auto.py -data mosaic_auto_pri_2 -checkpoints FPN_epoch_400_Mar01_14_21.pth -batch_size 22 -all_results False;
+done
+echo "end"
